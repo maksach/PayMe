8704ab57d17789e59784056f893b7bf4eb42a322
Trevor Halvorson <trevor.halvorson@gmail.com>
2016-05-18 18:57:55 -0700

    Initial commit

---
+++ app/src/main/java/edu/uw/t23/payme/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.uw.t23.payme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
